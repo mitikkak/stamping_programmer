cd59cbf0d4f31f936057bc55a937ed0337198345
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2019-04-17 12:52:40 +0300

    12e pins to use nodemcu pin names

--- src/Pins_12e.h
+++ src/Pins_12e.h
@@ -2,9 +2,9 @@
 #pragma once
 namespace stamping
 {
-static const int selectPin{2};
-static const int a1Pin{15};
-static const int a2Pin{14};
-static const int a3Pin{13};
-static const int a4Pin{12};
+static const int selectPin{D4};
+static const int a1Pin{D5};
+static const int a2Pin{D6};
+static const int a3Pin{D7};
+static const int a4Pin{D8};
 }
